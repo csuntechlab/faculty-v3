f89558da9c3a713fbe5ebd777c1a95529af42b5e
Matthew Fritz <matthew.fritz@csun.edu>
2018-10-18 09:57:11 -0700

    Removed unneeded code

--- resources/views/pages/search-results.blade.php
+++ resources/views/pages/search-results.blade.php
@@ -143,23 +143,6 @@
                 <div class="col-4">
                     <nav aria-label="Page navigation example">
                     {!! $people->links() !!}
-                    {{-- <ul class="pagination pagination-lg justify-content-center">
-                        <li class="page-item pr-2">
-                            <a class="page-link" href="#" aria-label="Previous">
-                                <span aria-hidden="true">&laquo;</span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                        </li>
-                        <li class="page-item px-2"><a class="page-link" href="#">1</a></li>
-                        <li class="page-item px-2"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item px-2"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item pl-2">
-                            <a class="page-link" href="#" aria-label="Next">
-                                <span aria-hidden="true">&raquo;</span>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        </li>
-                    </ul>--}}
                     </nav>
                 </div>
             </div>
