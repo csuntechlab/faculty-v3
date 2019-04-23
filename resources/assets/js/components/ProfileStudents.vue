02b382626f5be191212a9c92a93a95718b7283ed
Andrew Grano <andrew.grano@metalab.csun.edu>
2019-04-23 15:24:35 -0700

    add empty state message

--- resources/assets/js/components/ProfileStudents.vue
+++ resources/assets/js/components/ProfileStudents.vue
@@ -17,86 +17,89 @@
                             <h2 class="h3 text-primary mb-4">Students</h2>
                             <h3 class="font-display">Completed Master Theses:</h3>
                         </div>
-                    
-                        <div class="profileCitationWrapper">
-                            <div v-for="(citation, index) in orderedCitations" class="profileCitation card card--styled d-block mb-3" :data-type="citation.type" :id="'profileCitation--'+index">
-                                <div class="card-header bg-white" :id="'heading--'+index" data-toggle="collapse" :data-target="'#collapse--'+index" aria-expanded="false" :aria-controls="'#collapse--'+index">
-                                    <div class="profileCitation__citation">
-                                        <template v-if="citation.document.handle.split('http://hdl.handle.net')[1]">
-                                            <a @click.stop :href="'http://scholarworks.csun.edu/handle' + citation.document.handle.split('http://hdl.handle.net')[1]" target="scholarworks">
+                        <template v-if="citations.length">
+                            <div class="profileCitationWrapper">
+                                <div v-for="(citation, index) in orderedCitations" class="profileCitation card card--styled d-block mb-3" :data-type="citation.type" :id="'profileCitation--'+index">
+                                    <div class="card-header bg-white" :id="'heading--'+index" data-toggle="collapse" :data-target="'#collapse--'+index" aria-expanded="false" :aria-controls="'#collapse--'+index">
+                                        <div class="profileCitation__citation">
+                                            <template v-if="citation.document.handle.split('http://hdl.handle.net')[1]">
+                                                <a @click.stop :href="'http://scholarworks.csun.edu/handle' + citation.document.handle.split('http://hdl.handle.net')[1]" target="scholarworks">
+                                                    <template v-if="citation.formatted != ''">
+                                                        <span v-html="citation.formatted"></span>
+                                                    </template>
+                                                    <template v-else>
+                                                        <span v-html="citation.metadata.title"></span>
+                                                    </template>
+                                                    <sup><i class="fas fa-external-link-alt"></i></sup>
+                                                </a>
+                                            </template>
+                                            <template v-else>
                                                 <template v-if="citation.formatted != ''">
                                                     <span v-html="citation.formatted"></span>
                                                 </template>
                                                 <template v-else>
                                                     <span v-html="citation.metadata.title"></span>
                                                 </template>
-                                                <sup><i class="fas fa-external-link-alt"></i></sup>
-                                            </a>
-                                        </template>
-                                        <template v-else>
-                                            <template v-if="citation.formatted != ''">
-                                                <span v-html="citation.formatted"></span>
                                             </template>
-                                            <template v-else>
-                                                <span v-html="citation.metadata.title"></span>
+                                        </div>
+                                        <span class="text-capitalize">
+                                            Chair:
+                                        </span>
+                                        <a @click.stop class="text-underline text-body" :href="'/' + citation.membership.members[1].email.split('@')[0] + '#/students'">{{citation.membership.members[1].display_name}}</a>
+                                        
+
+                                        <span class="text-capitalize pl-2">
+                                            Members:
+                                        </span>
+                                        <span>
+                                            <template v-for="(member, index) in citation.membership.members">
+                                                <template v-if="index > 1">
+                                                    <a @click.stop class="text-underline text-body" :href="'/' + member.email.split('@')[0] + '#/students'">{{member.display_name}}</a><template v-if="index !== citation.membership.members.length-1">, </template>
+                                                </template>
                                             </template>
-                                        </template>
-                                    </div>
-                                    <span class="text-capitalize">
-                                        Chair:
-                                    </span>
-                                    <a @click.stop class="text-underline text-body" :href="'/' + citation.membership.members[1].email.split('@')[0] + '#/students'">{{citation.membership.members[1].display_name}}</a>
-                                    
+                                        </span>
 
-                                    <span class="text-capitalize pl-2">
-                                        Members:
-                                    </span>
-                                    <span>
-                                        <template v-for="(member, index) in citation.membership.members">
-                                            <template v-if="index > 1">
-                                                <a @click.stop class="text-underline text-body" :href="'/' + member.email.split('@')[0] + '#/students'">{{member.display_name}}</a><template v-if="index !== citation.membership.members.length-1">, </template>
-                                            </template>
-                                        </template>
-                                    </span>
-
-                                    <div class="d-flex justify-content-center justify-content-sm-end align-items-center flex-wrap">
-                                        <div>
-                                            <div class="btn btn-outline-dark btn-sm btn-rounded mt-3 mt-sm-0"><span class="is--collapsed">Show</span><span class="is--expanded">Hide</span> Details <i class="fas fa-chevron-down"></i></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div :id="'collapse--'+index" class="collapse" :aria-labelledby="'heading--'+index">
-                                    <div class="card-body">
-                                        <div class="mt-4 mb-3" v-if="citation.metadata.title">
-                                            <div class="font-weight-bold">Title:</div>
-                                            <span>{{citation.metadata.title}}</span>
-                                        </div>
-
-                                        <div class="mt-4 mb-3" v-if="citation.published.date">
-                                            <div class="font-weight-bold">Date: </div>
-                                            {{returnCitationDate(citation.published.date)}}
-                                        </div>
-
-                                        <div class="mt-4 mb-3" v-if="citation.metadata.journal">
-                                            <div class="font-weight-bold">Journal:</div>
-                                            <span>{{citation.metadata.journal }}</span>
-                                        </div>
-
-                                        <template v-if="citation.publisher">
-                                            <div class="mt-4 mb-3" v-if="citation.publisher.publisher">
-                                                <div class="font-weight-bold">Publisher:</div>
-                                                <span>{{citation.publisher.publisher }}</span>
+                                        <div class="d-flex justify-content-center justify-content-sm-end align-items-center flex-wrap">
+                                            <div>
+                                                <div class="btn btn-outline-dark btn-sm btn-rounded mt-3 mt-sm-0"><span class="is--collapsed">Show</span><span class="is--expanded">Hide</span> Details <i class="fas fa-chevron-down"></i></div>
                                             </div>
-                                        </template>
+                                        </div>
+                                    </div>
+                                    <div :id="'collapse--'+index" class="collapse" :aria-labelledby="'heading--'+index">
+                                        <div class="card-body">
+                                            <div class="mt-4 mb-3" v-if="citation.metadata.title">
+                                                <div class="font-weight-bold">Title:</div>
+                                                <span>{{citation.metadata.title}}</span>
+                                            </div>
 
-                                        <div class="mt-4 mb-3" v-if="citation.metadata.abstract">
-                                            <div class="font-weight-bold">Abstract:</div>
-                                            <span>{{ citation.metadata.abstract }}</span>
+                                            <div class="mt-4 mb-3" v-if="citation.published.date">
+                                                <div class="font-weight-bold">Date: </div>
+                                                {{returnCitationDate(citation.published.date)}}
+                                            </div>
+
+                                            <div class="mt-4 mb-3" v-if="citation.metadata.journal">
+                                                <div class="font-weight-bold">Journal:</div>
+                                                <span>{{citation.metadata.journal }}</span>
+                                            </div>
+
+                                            <template v-if="citation.publisher">
+                                                <div class="mt-4 mb-3" v-if="citation.publisher.publisher">
+                                                    <div class="font-weight-bold">Publisher:</div>
+                                                    <span>{{citation.publisher.publisher }}</span>
+                                                </div>
+                                            </template>
+                                            <div class="mt-4 mb-3" v-if="citation.metadata.abstract">
+                                                <div class="font-weight-bold">Abstract:</div>
+                                                <span>{{ citation.metadata.abstract }}</span>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </template>
+                        <template v-else>
+                            <div class="mb-2">There are currently no theses to display.</div>
+                        </template>
                     </div>
                 </div>
             </template>
